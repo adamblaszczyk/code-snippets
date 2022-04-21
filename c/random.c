0e5601b18948a7ff279c230b5cb072fce5f751f4
Adam Błaszczyk <blaszczyk.adam@gmail.com>
2022-04-21 08:48:23 +0200

    Add files via upload

---
+++ c/random.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int getRandomNumber(int maxNumber) {
+  int randomNumber = rand() % maxNumber + 1;
+  return randomNumber;
+}
+
+int main() {
+  srand(time(NULL));
+  for(int i = 0; i < 50; i++) {
+    int randomNumber = getRandomNumber(100); //1-100
+    printf("%d\n", randomNumber);
+  }
+}
