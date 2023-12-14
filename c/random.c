443a5a32e1e7456108fc6bb895b883bc6fc781e3
Adam Błaszczyk <blaszczyk.adam@gmail.com>
2023-12-14 22:54:17 +0100

    Update random.c

--- c/random.c
+++ c/random.c
@@ -3,14 +3,14 @@
 #include <time.h>
 
 int getRandomNumber(int maxNumber) {
-  int randomNumber = rand() % maxNumber + 1;
-  return randomNumber;
+    int randomNumber = rand() % maxNumber + 1;
+    return randomNumber;
 }
 
 int main() {
-  srand(time(NULL));
-  for(int i = 0; i < 50; i++) {
-    int randomNumber = getRandomNumber(100); //1-100
-    printf("%d\n", randomNumber);
-  }
+    srand(time(NULL));
+    for(int i = 0; i < 50; i++) {
+        int randomNumber = getRandomNumber(100); //1-100
+        printf("%d\n", randomNumber);
+    }
 }
